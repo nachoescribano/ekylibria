c05f08a0e641644eb16a7600df90c8de8bc3d618
anotero <andres.otero16@gmail.com>
2025-01-03 00:03:19 -0300

    agregado de gulp + gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,36 @@ const zip = require('gulp-zip');
 const clean = require('gulp-clean');
 const replace = require('gulp-replace');
 const dateFormat = require('dateformat');
+const fs = require('fs');
+const log = require('fancy-log');
+const colors = require('ansi-colors');
+const https = require('https');
+const decompress = require('gulp-decompress');
 
-// Configuración
 const config = {
   buildDir: './build',
-  zipName: () => `site-build-${dateFormat(new Date(), "yyyy-mm-dd-HHMMss")}.zip`,
-  // Archivos a incluir
-  includes: [
-    '**/*',
+  tempDir: './temp',
+  buildsDir: './builds',
+  zipName: (type) => `site-${type}-${dateFormat(new Date(), "yyyy-mm-dd-HHMMss")}.zip`,
+  wpVersion: '6.7.1', // Especifica la versión de WordPress
+  
+  // Configuración para build de actualización
+  updateIncludes: [
     'wp-content/**/*',
-    'wp-content/themes/**/*',
-    'wp-content/plugins/**/*',
   ],
-  // Archivos a excluir
+  
+  // Configuración para build inicial
+  fullIncludes: [
+    'wp-admin/**/*',
+    'wp-includes/**/*',
+    'wp-content/**/*',
+    'index.php',
+    'wp-*.php',
+    '.htaccess',
+    'wp-config-sample.php'
+  ],
+  
+  // Exclusiones comunes
   excludes: [
     '!node_modules/**',
     '!build/**',
@@ -25,30 +42,100 @@ const config = {
     '!.git/**',
     '!wp-config.php',
     '!.env',
-    '!README.md'
+    '!README.md',
+    '!**/*.log'
   ]
 };
 
-// Limpiar directorio build
+// Limpiar directorios temporales
 gulp.task('clean', () => {
-  return gulp.src(config.buildDir, { allowEmpty: true })
+  return gulp.src([config.buildDir, config.tempDir], { allowEmpty: true })
     .pipe(clean({ force: true }));
 });
 
-// Copiar archivos al build
-gulp.task('copy', () => {
-  return gulp.src([...config.includes, ...config.excludes])
-    .pipe(gulp.dest(config.buildDir));
+// Crear directorios necesarios
+gulp.task('init', (done) => {
+  // Creamos buildDir y tempDir si no existen
+  [config.buildDir, config.tempDir].forEach(dir => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+      log(colors.green(`Directorio creado: ${dir}`));
+    }
+  });
+  
+  // Creamos buildsDir si no existe (pero no lo limpiamos)
+  if (!fs.existsSync(config.buildsDir)) {
+    fs.mkdirSync(config.buildsDir, { recursive: true });
+    log(colors.green(`Directorio de builds creado: ${config.buildsDir}`));
+  }
+  
+  done();
 });
 
-// Crear ZIP
-gulp.task('zip', () => {
-  return gulp.src(`${config.buildDir}/**/*`)
-    .pipe(zip(config.zipName()))
-    .pipe(gulp.dest('./'));
+// Descargar y descomprimir WordPress
+gulp.task('download-wp', (done) => {
+  const wpUrl = `https://wordpress.org/wordpress-${config.wpVersion}.zip`;
+  const filePath = `${config.tempDir}/wordpress.zip`;
+  
+  log(colors.blue(`Descargando WordPress ${config.wpVersion}...`));
+  
+  const file = fs.createWriteStream(filePath);
+  https.get(wpUrl, (response) => {
+    if (response.statusCode !== 200) {
+      log.error(colors.red(`Error descargando WordPress: ${response.statusCode}`));
+      done(new Error(`HTTP Status Code: ${response.statusCode}`));
+      return;
+    }
+    
+    response.pipe(file);
+    
+    file.on('finish', () => {
+      file.close();
+      log(colors.green('WordPress descargado correctamente'));
+      
+      // Descomprimir WordPress
+      gulp.src(`${config.tempDir}/wordpress.zip`)
+        .pipe(decompress())
+        .pipe(gulp.dest(config.tempDir))
+        .on('end', () => {
+          // Mover archivos desde wordpress/ al buildDir
+          gulp.src(`${config.tempDir}/wordpress/**/*`)
+            .pipe(gulp.dest(config.buildDir))
+            .on('end', done);
+        });
+    });
+  }).on('error', (err) => {
+    fs.unlink(filePath, () => {}); // Limpiar archivo parcial
+    log.error(colors.red(`Error descargando WordPress: ${err.message}`));
+    done(err);
+  });
 });
 
-// Tarea principal
-gulp.task('build', 
-  gulp.series('clean', 'copy', 'zip')
-); 
+// Build de actualización (solo wp-content)
+gulp.task('build-update', 
+  gulp.series('clean', () => {
+    return gulp.src([...config.updateIncludes, ...config.excludes])
+      .pipe(gulp.dest(config.buildDir))
+      .pipe(zip(config.zipName('update')))
+      .pipe(gulp.dest('./builds'));
+  })
+);
+
+// Build completo (instalación inicial)
+gulp.task('build-full', 
+  gulp.series('clean', 'init', 'download-wp', () => {
+    return gulp.src([...config.fullIncludes, ...config.excludes], { base: '.' })
+      .pipe(gulp.dest(config.buildDir))
+      .pipe(zip(config.zipName('full')))
+      .pipe(gulp.dest(config.buildsDir));
+  })
+);
+
+// Tarea por defecto
+gulp.task('default', (done) => {
+  log(colors.blue('Tareas disponibles:'));
+  log(colors.yellow('gulp build-update') + ' - Genera build de actualización');
+  log(colors.yellow('gulp build-full') + ' - Genera build completo con WordPress');
+  log(colors.yellow('gulp clean') + ' - Limpia directorios temporales');
+  done();
+}); 
